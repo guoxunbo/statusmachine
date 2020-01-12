6a4ded657734ebb682e1b3ef005c44d2deadefd4
guoxunbo <11603652@qq.com>
2020-01-12 11:31:33 +0800

    去除无用代码

--- src/main/java/com/newbiest/commom/sm/StatusMachineConfiguration.java
+++ src/main/java/com/newbiest/commom/sm/StatusMachineConfiguration.java
@@ -2,34 +2,18 @@ package com.newbiest.commom.sm;
 
 import com.newbiest.base.factory.ModelFactory;
 import com.newbiest.commom.sm.model.EventStatus;
-import com.newbiest.main.YmlPropertyLoaderFactory;
 import liquibase.integration.spring.SpringLiquibase;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnResource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
-/**
- * Created by guoxunbo on 2019/1/10.
- */
-@Configuration
-@ConfigurationProperties(prefix = "status-machine.liquibase")
-@Data
-@PropertySource(value = "classpath:status-machine.yml", factory = YmlPropertyLoaderFactory.class)
 @Slf4j
+@Component
 public class StatusMachineConfiguration {
-
-    private String changeLog;
-
-    private boolean enabled;
-
-    private boolean dropFirst;
 
     /**
      * 默认的LiquibaseBean
@@ -38,16 +22,15 @@ public class StatusMachineConfiguration {
      * @throws Exception
      */
     @Bean("statusMachineLiquibase")
-    @ConditionalOnResource(resources = {"classpath:status-machine.yml"})
     public SpringLiquibase liquibase(DataSource dataSource) throws Exception{
         if (log.isInfoEnabled()) {
             log.info("Load StatusMachine Liquibase Configuration.");
         }
         SpringLiquibase liquibase = new SpringLiquibase();
         liquibase.setDataSource(dataSource);
-        liquibase.setChangeLog(changeLog);
-        liquibase.setShouldRun(enabled);
-        liquibase.setDropFirst(dropFirst);
+        liquibase.setChangeLog("classpath:db/changelog/db.changelog-statusmachine.yaml");
+        liquibase.setShouldRun(true);
+        liquibase.setDropFirst(false);
         return liquibase;
     }
 
